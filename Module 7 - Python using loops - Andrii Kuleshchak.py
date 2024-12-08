88ce0858a7bdd6d869beede1df22d2e8a463687a
akuleshchak <akuleshchak@ivytech.edu>
2024-12-07 22:04:44 -0500

    Add files via upload

---
+++ Module 7 - Python using loops - Andrii Kuleshchak.py
@@ -0,0 +1,7 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+for num in numbers:
+    if (num % 2 == 0):
+        print("The number ", str(num), " is even.")
+    else:
+        print("The number ", str(num), "is odd.")
