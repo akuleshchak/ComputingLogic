d626fbfb71053d9f6a8ff3b01b6d6821112cb44a
akuleshchak <akuleshchak@ivytech.edu>
2024-12-09 12:06:38 -0500

    Add files via upload

---
+++ Module 8 - Functions in Python.py
@@ -0,0 +1,8 @@
+def greaterThan(x, y):
+    if x > y: return True
+    else: return False
+
+a = 10
+b = 6
+result = greaterThan(a, b)
+print("The statement", str(a), "is greater than", str(b), "is", str(result))
